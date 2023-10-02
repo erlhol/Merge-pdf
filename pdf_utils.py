68bf0ef5db5f1e1db2aeb6b4660914a1742757d9
Erling Holte <erling.holte@outlook.com>
2023-10-02 19:41:01 +0200

    Add downloads to merge/directory

--- pdf_utils.py
+++ pdf_utils.py
@@ -27,14 +27,34 @@ def files_to_directory(directory_name: str | Path, dest_path: str | Path):
         i += 1
 
 def files_from_downloads_to_directory(dest_path: str | Path):
+    dest_path: Path = Path(dest_path)
     if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
         downloads_path = Path(Path.home() / "Downloads")
-        print(downloads_path)
-        for content in downloads_path.rglob("*.pdf"):
-            print(content)
+        dest_path.mkdir()
+        i = 0
+        for pdf in downloads_path.rglob("*.pdf"):
+            filename = str(i) + '.pdf'
+            new_dest_path = dest_path / Path(filename)
+            shutil.move(pdf,new_dest_path)
+            i += 1
     else:
-        print("GTFO")
+        print("Your loss, get a real computer")
+
+def files_from_downloads_to_merge(mergename: str | Path):
+    merge_p = Path(mergename)
+    if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
+        assert merge_p.suffix == '.pdf'
+        downloads_path = Path(Path.home() / "Downloads")
+        merger = PdfMerger()
+        for pdf in sorted(downloads_path.rglob("*.pdf"),key=os.path.getmtime):
+            merger.append(pdf)     
+
+        merger.write(mergename)
+        merger.close()
+    else:
+        print("Your loss, get a real computer")
 
 #files_to_one_pdf('myfiles','merged.pdf')
 #files_to_directory('myfiles','my_dir')
-files_from_downloads_to_directory("")
+#files_from_downloads_to_directory("mine_filer")
+files_from_downloads_to_merge("mymerge.pdf")
